b879c1550f0ff1e97864e969e53ace72d9cca9bf
Mario-Fernando24 <3115408069.mafe@gmail.com>
2023-10-24 02:03:09 -0500

    mostrar todas las noticias

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsService } from '../../services/news.service';
-// import { NewsResponse } from '../../interface/index';
+import { Article } from '../../interface';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
+
+  public article: Article[] = []
 
   //inyectamos
   constructor(private NewsService: NewsService) { }
@@ -16,6 +18,8 @@ export class Tab1Page implements OnInit {
     this.NewsService.getNewsHeadLines()
       .subscribe(( resp) => {
         console.log(resp);
+        //desestructuracion
+         this.article.push(...resp)
       });
 
   }
