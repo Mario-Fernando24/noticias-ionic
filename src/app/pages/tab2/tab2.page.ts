e54d4824c141cbc24a12e2f2ecba99efd3bb0527
Mario-Fernando24 <3115408069.mafe@gmail.com>
2023-10-25 13:48:21 -0500

    filtrar por categoria

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,22 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../services/news.service';
+import { Article } from '../../interface';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
   public categories: string[] = ['business','entertainment','general','health','science','sports','technology']
   public selectedCategory: string = this.categories[0];
   
+  public article: Article[] = []
 
-  constructor() { }
+  constructor(private newsService: NewsService) { }
+
+  ngOnInit(): void {
+      this.newsService.getToHeadLinesByCategory(this.selectedCategory)
+      .subscribe(article =>{
+        //this.article.push(...article)    
+        this.article = [...this.article, ...article]    
+      })
+  }
 
   //recibo una categoria
   segmentChanged(categoria: any) {
     this.selectedCategory = categoria.detail.value;
     console.log(categoria.detail.value);
+    this.newsService.getToHeadLinesByCategory(this.selectedCategory)
+    .subscribe(article =>{
+      this.article = [...article]       
+    })
 
   }
 
