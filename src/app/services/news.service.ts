988c95fd9e7147be3a03a5b43b84ff66fb700b16
Mario-Fernando24 <3115408069.mafe@gmail.com>
2023-10-25 15:03:28 -0500

    fix services NewsResponse

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -15,28 +15,27 @@ export class NewsService {
   constructor(private http: HttpClient) { }
 
   // centralizar generico T K
-  private executeQuery<T>(endpoint: string){
-      return this.http.get<any>(`${apiUrl}${endpoint} `,{
-        params: {apiKey}
-      })
+  private executeQuery<T>(endpoint: string) {
+    return this.http.get<any>(`${apiUrl}${endpoint} `, {
+      params: { apiKey }
+    })
   }
 
   getNewsHeadLines(): Observable<Article[]> {
 
-    return this.executeQuery(`/top-headlines?country=us&category=business`).
+    return this.executeQuery<NewsResponse>(`/top-headlines?country=us&category=business`).
       pipe(
-        map(({resp}) => resp));
+        map(({ resp }) => resp));
   }
 
 
   getToHeadLinesByCategory(categoria: string): Observable<Article[]> {
 
-    return this.http.get<NewsResponse>(`https://newsapi.org/v2/top-headlines?country=us&category=${categoria}`,{
-      params: {apiKey}
-    }).
+
+    return this.executeQuery<NewsResponse>(`top-headlines?country=us&category=${categoria}`).
       pipe(
-        map(resp => resp.articles)
-      );
+        map(({ resp }) => resp.articles));
+
   }
 
 
